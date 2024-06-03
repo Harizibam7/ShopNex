e5d4bd035da40b4d851b7fb19387d3de66d451d5
Harizibam7 <harizibamv@gmail.com>
2024-06-03 23:03:54 +0530

    Collections Changed

--- src/Components/NewCollections/NewCollections.jsx
+++ src/Components/NewCollections/NewCollections.jsx
@@ -12,7 +12,7 @@ const NewCollections = () => {
             <hr className={`hr_${theme}`} />
             <div className="collections">
             {new_collection.map((item,i)=>{
-                return <Item key={i}   id={item.id}  name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+                return <Item key={i}   id={item.id}  name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}  />
             })}
             </div>
 
